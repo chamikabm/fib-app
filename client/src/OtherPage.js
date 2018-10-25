8a23f3c341b05656d66946321fac5e0f7b7d3ba2
chamikab <kasun.chamika@gmail.com>
2018-10-26 02:02:39 +0530

    Added client react app changed and gitignore file..

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const otherPage = ()=> {
+
+  return (
+    <div>
+      I'm some other page!!
+    </div>
+  );
+}
+
+export default otherPage;
