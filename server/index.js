4a13238e8e1cc1e3710ed4ee4eaa7fc2fbeb8f0b
chamikab <kasun.chamika@gmail.com>
2018-10-26 01:24:35 +0530

    Added express routings to connect react app and backend server.

--- server/index.js
+++ server/index.js
@@ -23,4 +23,44 @@ pgClient
   .query('CREATE TABLE IF NOT EXIST values(number INT)')
   .catch(error => console.log(error));
 
+// Redis Client setup.
+const redis = require('redis');
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
 
+// Express Route Handlers.
+app.get('/', (req, res) => {
+  res.send('Hi!')
+});
+
+app.get('/values/all', async (req, res) => {
+  const values = await pgClient.query('SELECT * FROM values');
+  res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+  await redisClient.hgetAll('values', (error, values) => {
+    res.send(values);
+  });
+});
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
+  if (parseInt(index) > 40) {
+    return res.status(422).send('Index too high');
+  }
+
+  redisClient.hset('values', index, 'Nothing yet!');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, () => {
+  console.log('Started listing on port 5000!!');
+})
