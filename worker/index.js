83fb2e54a52c94ff017dcdd65d521785edf913bb
chamikab <kasun.chamika@gmail.com>
2018-10-25 23:33:53 +0530

    Added changes for calculating fib value from the worker.

--- worker/index.js
+++ worker/index.js
@@ -10,3 +10,18 @@ const redisClient = redis.createClient({
 });
 
 const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) {
+    return 1;
+  }
+
+  return fib(index-1) + fib(index -2);
+}
+
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
+});
+
+sub.subscribe('insert')
+
